add2aed74cff9114e983708672609975bd429ef8
FarmerChina <ningguotong1142115009@outlook.com>
2023-01-29 17:19:17 +0800

    Initial commit

---
+++ Example/MyTestPodLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyTestPodLib
+//
+//  Created by FarmerChina on 01/29/2023.
+//  Copyright (c) 2023 FarmerChina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
